0aeaf2df09ccc87175632826c5c02bacfb2218b5
Rxphh <raphael.cornet.w@gmail.com>
2024-12-15 00:07:30 +0100

    correction of the computer ai

--- player.py
+++ player.py
@@ -1,8 +1,7 @@
 from random import choice
 from typing import Callable, Optional
 
-from constants import GRID_SIDE
-from grid import valid, add, convert, possibles, finish, remove
+from grid import add, convert, finish, possibles, remove, valid
 
 
 def __ask(grid: list[list[int]]) -> int:
@@ -14,22 +13,38 @@ def __ask(grid: list[list[int]]) -> int:
     return int(move)
 
 
-def __corresponding(player: int) -> Callable[[list[int]], int]:
-    return max if player == 0 else min
+def __best(grid) -> tuple[int, int]:
+    best_placement: Optional[tuple[int, int]] = None
+    best_score: float = float("-inf")
+    for placement in possibles(grid):
+        add(grid, placement, 2)
+        score: float = __eval(grid, 0)
+        remove(grid, placement)
+        print(score)
+        if score > best_score:
+            best_score = score
+            best_placement = placement
+    return best_placement
 
 
-def __other(player: int) -> int:
-    return 0 if player == 2 else 2
+def __corresponding(player: int) -> Callable[[float, float], float]:
+    return max if player == 2 else min
 
 
 def __default_score(player: int) -> float:
-    return float("-inf") if player == 0 else float("+inf")
+    return float("-inf") if player == 2 else float("+inf")
 
 
-def __eval(grid, player) -> float:
+def __eval(grid: list[list[int]], player: int) -> float:
     result: tuple[bool, Optional[int]] = finish(grid)
     if result[0]:
-        return result[1]
+        if result[1] == 2:
+            return 1
+        elif result[1] == 0:
+            print("a")
+            return -1
+        else:
+            return 0
     best_score: float = __default_score(player)
     for placement in possibles(grid):
         add(grid, placement, player)
@@ -38,25 +53,14 @@ def __eval(grid, player) -> float:
         best_score = __corresponding(player)(best_score, score)
     return best_score
 
-def __best(grid) -> tuple[int, int]:
-    best_placement: Optional[tuple[int, int]] = None
-    best_score: float = float("-inf")
-    for placement in possibles(grid):
-        add(grid, placement, 2)
-        score: float = __eval(grid, 0)
-        add(grid, placement, 1)
-        if score > best_score:
-            best_score = score
-            best_placement = placement
-    return best_placement
+
+def __other(player: int) -> int:
+    return 0 if player == 2 else 2
 
 
-def computer_turn(grid: list[list[int]], turn: int):
-    if turn == 0:
-        add(grid, (GRID_SIDE // 2, GRID_SIDE // 2), 2)
-        return
-    placement: tuple[int, int] = __best(grid)
-    add(grid, placement, 2)
+def computer_turn(grid: list[list[int]]):
+    best_move = __best(grid)
+    add(grid, best_move, 2)
 
 
 def human_turn(grid: list[list[int]]):
